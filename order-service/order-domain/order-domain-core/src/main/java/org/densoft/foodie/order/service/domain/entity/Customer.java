240f3c3946089f1cd85e88978c7e98dddf51cf56
dennis githinji <wakahiad@gmail.com>
2023-06-09 19:56:10 +0300

    adding state changing methods to order entity, create Restaurant and Customer aggregate

---
+++ order-service/order-domain/order-domain-core/src/main/java/org/densoft/foodie/order/service/domain/entity/Customer.java
@@ -0,0 +1,8 @@
+package org.densoft.foodie.order.service.domain.entity;
+
+import org.densoft.foodie.domain.entity.AggregateRoot;
+import org.densoft.foodie.domain.value.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+
+}
