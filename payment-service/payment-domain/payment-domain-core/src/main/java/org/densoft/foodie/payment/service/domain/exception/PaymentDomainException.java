8d5ab614885c08b3ec465bfc48b89558dcb81725
dennis githinji <wakahiad@gmail.com>
2023-06-24 17:59:36 +0300

    Domain core module adding Exception and Domain events

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/org/densoft/foodie/payment/service/domain/exception/PaymentDomainException.java
@@ -0,0 +1,13 @@
+package org.densoft.foodie.payment.service.domain.exception;
+
+import org.densoft.foodie.domain.exception.DomainException;
+
+public class PaymentDomainException extends DomainException {
+    public PaymentDomainException(String message) {
+        super(message);
+    }
+
+    public PaymentDomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
