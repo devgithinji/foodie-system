f559fc4b25b327f1366a59404645ca2804bef5d7
dennis githinji <wakahiad@gmail.com>
2023-06-29 15:06:07 +0300

    domain core module adding exception domain events and domain service

---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/org/densoft/foodie/retaurant/service/domain/RestaurantDomainServiceImpl.java
@@ -0,0 +1,44 @@
+package org.densoft.foodie.retaurant.service.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.densoft.foodie.domain.event.publisher.DomainEventPublisher;
+import org.densoft.foodie.domain.value.OrderApprovalStatus;
+import org.densoft.foodie.retaurant.service.domain.entity.Restaurant;
+import org.densoft.foodie.retaurant.service.domain.event.OrderApprovalEvent;
+import org.densoft.foodie.retaurant.service.domain.event.OrderApprovedEvent;
+import org.densoft.foodie.retaurant.service.domain.event.OrderRejectedEvent;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.densoft.foodie.domain.DomainConstants.UTC;
+
+@Slf4j
+public class RestaurantDomainServiceImpl implements RestaurantDomainService {
+    @Override
+    public OrderApprovalEvent validateOrder(Restaurant restaurant,
+                                            List<String> failureMessages,
+                                            DomainEventPublisher<OrderApprovedEvent> orderApprovedEventDomainEventPublisher,
+                                            DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher) {
+        restaurant.validateOrder(failureMessages);
+        log.info("Validating order with id: {}", restaurant.getOrderDetail().getId().getValue());
+        if (failureMessages.isEmpty()) {
+            log.info("Order is approved for order id: {}", restaurant.getOrderDetail().getId().getValue());
+            restaurant.constructOrderApproval(OrderApprovalStatus.APPROVED);
+            return new OrderApprovedEvent(restaurant.getOrderApproval(),
+                    restaurant.getId(),
+                    failureMessages,
+                    ZonedDateTime.now(ZoneId.of(UTC)),
+                    orderApprovedEventDomainEventPublisher);
+        } else {
+            log.info("Order is rejected for order id: {}", restaurant.getOrderDetail().getId().getValue());
+            restaurant.constructOrderApproval(OrderApprovalStatus.REJECTED);
+            return new OrderRejectedEvent(restaurant.getOrderApproval(),
+                    restaurant.getId(),
+                    failureMessages,
+                    ZonedDateTime.now(ZoneId.of(UTC)),
+                    orderRejectedEventDomainEventPublisher);
+        }
+    }
+}
---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/org/densoft/foodie/retaurant/service/domain/event/OrderApprovedEvent.java
@@ -0,0 +1,26 @@
+package org.densoft.foodie.retaurant.service.domain.event;
+
+import org.densoft.foodie.domain.event.publisher.DomainEventPublisher;
+import org.densoft.foodie.domain.value.RestaurantId;
+import org.densoft.foodie.retaurant.service.domain.entity.OrderApproval;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class OrderApprovedEvent extends OrderApprovalEvent {
+
+    private final DomainEventPublisher<OrderApprovedEvent> orderApprovalEventDomainEventPublisher;
+
+    public OrderApprovedEvent(OrderApproval orderApproval,
+                              RestaurantId restaurantId,
+                              List<String> failureMessages,
+                              ZonedDateTime createdAt, DomainEventPublisher<OrderApprovedEvent> orderApprovalEventDomainEventPublisher) {
+        super(orderApproval, restaurantId, failureMessages, createdAt);
+        this.orderApprovalEventDomainEventPublisher = orderApprovalEventDomainEventPublisher;
+    }
+
+    @Override
+    public void fire() {
+        orderApprovalEventDomainEventPublisher.publish(this);
+    }
+}
