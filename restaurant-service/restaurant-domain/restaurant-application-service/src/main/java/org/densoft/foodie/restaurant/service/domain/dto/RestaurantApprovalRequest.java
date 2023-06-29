ffb31c2afdec2f8f7e77582a411cbdcc2a797a44
dennis githinji <wakahiad@gmail.com>
2023-06-29 22:23:31 +0300

    implementing messaging module

--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/org/densoft/foodie/restaurant/service/domain/dto/RestaurantApprovalRequest.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/org/densoft/foodie/restaurant/service/domain/dto/RestaurantApprovalRequest.java
@@ -1,6 +1,7 @@
 package org.densoft.foodie.restaurant.service.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.densoft.foodie.domain.valueobject.RestaurantOrderStatus;
@@ -13,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class RestaurantApprovalRequest {
     private String id;
     private String  sagaId;
